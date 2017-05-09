75627bf8aed24dd1b79aefca072661f2770ca5dc
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2017-05-09 20:27:58 +0930

    Latest commit

--- Source/ChallengeExercise3.c
+++ Source/ChallengeExercise3.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
+void printNumbers(int a, int b, int counter);
 int main (void)
 {
 printf("\nPlease enter two integers, comma seperated:\n");
 int a = 0;
 int b = 0;
+int lowerBound = 0;
+int upperBound = 0;
 scanf("%d, %d",&a, &b);
 printf("\nThe two integers you have entered are: %d and %d\n\n",a, b);
-
-printNumbers(a,b); /* Recursive Function with a display of recusrive call counter */
+int counter = 0;
+if(a<b){
+ lowerBound = a;
+ upperBound = b;
+}
+if(a>b){
+ lowerBound = b;
+ upperBound = a;
+}
+printNumbers(lowerBound, upperBound, counter); /* Recursive Function with a display of recusrive call counter */
 
 
 
@@ -20,3 +31,10 @@ printNumbers(a,b); /* Recursive Function with a display of recusrive call counte
 
 
 }
+void printNumbers(int a, int b, int counter)
+{
+printf("a = %d, b =  %d, recursive call count =  %d\n", a,b,counter);
+if (a+1<=b)
+printNumbers((a+1), b, (counter+1));
+}
+
---
+++ Source/switchPlay.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(void)
+{
+    char trafficLightColor = '0';
+
+
+    switch(trafficLightColor){
+	case 'r':
+	    printf("Stop Driving \n");
+	case 'y':
+	    printf("Slow down if within 30m\n");
+	case 'g':
+	    printf("Go go go!\n");
+	default:
+	    printf("TrafficLight has malfunctioned\n");
+
+}
+
+
+
+
+
+
+
+
+
+}
