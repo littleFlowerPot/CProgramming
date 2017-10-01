602dd5d75f26df1be33b20209182c95eea486de7
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2017-10-01 14:59:30 +1030

    added comments to the CharacterCounting source

--- K&R/Chapter1:TutorialIntroduction/src/CharacterCounting.c
+++ K&R/Chapter1:TutorialIntroduction/src/CharacterCounting.c
@@ -11,4 +11,14 @@ main()
         ;
     printf("%.0f\n", nc);
 } 
+/*******************************************************
+Note here that, if EOF is encountered, nc is still set to 0.
+So initilaisation occurs before the stop condition is tested for.
+This is indeed mention in K&R: The initialization is done once, before
+the loop is entered proper.
 
+This example here is a good example of using for statement with an empty body
+to control the program, including dealing with the case of empty input (i.e EOF
+input straight away). All the work is done in the test and increment parts of the
+for statement.
+**********************************************************/
