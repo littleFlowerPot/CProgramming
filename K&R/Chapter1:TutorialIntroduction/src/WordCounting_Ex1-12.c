e8b828a027ca6af18cc097b8d2c57d53da457275
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2017-10-11 18:58:41 +1030

    started working on exercise 1-12

---
+++ K&R/Chapter1:TutorialIntroduction/src/WordCounting_Ex1-12.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*Exercise 1-12 */
+
+#define IN 1
+#define OUT 0
+
+main()
+{
+    int c, state;
+    state = OUT;
+    while((c = getchar()) != EOF)
+    {
+        
+    }
+}
+     
