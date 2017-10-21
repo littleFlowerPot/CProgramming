33d180b976cc575c916a5ab69de4bb990cc166f0
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2017-10-21 14:25:14 +1030

    Completed Exercise 1-12

--- K&R/Chapter1:TutorialIntroduction/src/WordCounting_Ex1-12.c
+++ K&R/Chapter1:TutorialIntroduction/src/WordCounting_Ex1-12.c
@@ -2,16 +2,32 @@
 
 /*Exercise 1-12 */
 
-#define IN 1
-#define OUT 0
+#define IN 1 /* inside a word */
+#define OUT 0 /* outside a word */
 
 main()
 {
-    int c, state;
+    int c, state, numberOfOuts;
     state = OUT;
+    numberOfOuts = 0;
     while((c = getchar()) != EOF)
     {
-        
+       if (c == ' ' || c == '\n' || c == '\t')
+       {   
+          state = OUT;
+          ++numberOfOuts;
+       }
+       else if (state == OUT)
+       { 
+          state = IN;
+          numberOfOuts = 0;
+       }             
+       if (state == IN) 
+          putchar(c);
+       else if (state == OUT && numberOfOuts == 1)
+       {
+          printf("\n");
+       }
     }
 }
      
