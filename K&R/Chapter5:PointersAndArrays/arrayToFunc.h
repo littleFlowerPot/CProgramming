f24ec9c274130035a0ca8d112806e29ae5d9835f
philippe@p-eeepc-1000h <p.rzetelski.note@gmail.com>
2018-02-26 01:51:38 +1030

    array name being passed to function

---
+++ K&R/Chapter5:PointersAndArrays/arrayToFunc.h
@@ -0,0 +1,8 @@
+int strlen_mine(char s[]);
+/* or ....(char *s), it does
+ * not matter, and you can
+ * mix the header declaration
+ * and the function definition
+ * freely (although perhaps
+ * could be poor style
+ */
