329b2008ff377b5244c4f526d1091055402d30e4
philippe@p-eeepc-1000h <p.rzetelski.note@gmail.com>
2018-02-26 01:55:00 +1030

    modified a comment

--- K&R/Chapter5:PointersAndArrays/arrayToFunc.h
+++ K&R/Chapter5:PointersAndArrays/arrayToFunc.h
@@ -3,6 +3,6 @@ int strlen_mine(char s[]);
  * not matter, and you can
  * mix the header declaration
  * and the function definition
- * freely (although perhaps
- * could be poor style
+ * freely (and pg 100 refers to mixing the two _within a
+ * function definition_ when it makes sense to do so.
  */
