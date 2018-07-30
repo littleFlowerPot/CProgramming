873f62141ebb49fb7debc8b96d0f75ee51d87742
CrazyPenguin <p.rzetelski.note@gmail.com>
2018-07-31 08:54:52 +0930

    listOperations header file created

---
+++ Algorithms/Lists/listOperations.h
@@ -0,0 +1,13 @@
+/* Current Assumptions:
+ * - List is at least size 1
+ * - List has integer data
+ */
+
+int listLength(struct ListNode* head);
+int getElement(struct ListNode* head, int i);
+struct ListNode* getNode(struct ListNode* head, int i);
+void deleteNode(struct ListNode* node);
+        
+
+                
+        
